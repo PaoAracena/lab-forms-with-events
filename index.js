0745dcd3c68cfc29fd62be61cb1ee9eb1f12ac66
paoAracena <Paolaaracena1233@gmail.com>
2023-03-19 17:32:49 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+const formElement = document.getElementById('pao')
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let listElement = document.createElement('li');
+    const textInputValue = event.target.testInput.value;
+    
+    listElement.textContent = textInputValue;
+    
+    listElement.addEventListener('click', handleClick =>{
+     handleClick.target.style.textDecoration = "line-through";
+    })
+
+    const unorderedListElement = document.querySelector('ul');
+    unorderedListElement.append(listElement);
+    //creation/ assigenment/functionally and then append
+    event.target.testInput.value ='';
+})
